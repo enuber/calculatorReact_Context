f0436bb9c420a0df8ec823970c4d693d9150e78f
Erik Nuber <erik.nuber@yahoo.com>
2020-12-08 16:25:57 -0800

    added title, url for build

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import { CalculatorStore } from '../contexts/CalculatorContext';
 const app = () => {
     return(
         <div className="app">
+            <h1 style={{textAlign: "center"}}>Calculator Using React Context</h1><br/>
             <CalculatorStore>
                 <Calculator/>
             </CalculatorStore>
